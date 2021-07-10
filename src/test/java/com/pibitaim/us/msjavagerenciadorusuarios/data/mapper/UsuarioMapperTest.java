27e67ce4c0993a54b53b27264b859531f3b1aae3
augustoluiz <augustogu7@yahoo.com.br>
2021-07-09 23:52:15 -0300

    Testes Unitários - TelefonesUsuarioMapperTest

--- src/test/java/com/pibitaim/us/msjavagerenciadorusuarios/data/mapper/UsuarioMapperTest.java
+++ src/test/java/com/pibitaim/us/msjavagerenciadorusuarios/data/mapper/UsuarioMapperTest.java
@@ -4,9 +4,11 @@ import com.pibitaim.us.msjavagerenciadorusuarios.data.dto.UsuarioDTO;
 import com.pibitaim.us.msjavagerenciadorusuarios.data.form.UsuarioForm;
 import com.pibitaim.us.msjavagerenciadorusuarios.data.form.UsuarioFormPerfil;
 import com.pibitaim.us.msjavagerenciadorusuarios.entity.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -31,41 +33,59 @@ public class UsuarioMapperTest {
     private final String EMAIL_CADASTRO_LOWER = "email@teste.com";
 
     @Mock
-    private Usuario usuario;
+    private Usuario mockUsuario;
 
     @Mock
-    private Page<Usuario> usuarios;
+    private Page<Usuario> mockUsuarios;
 
     @Mock
+    private UsuarioForm mockUsuarioForm;
+
+    @Mock
+    private UsuarioFormPerfil mockUsuarioFormPerfil;
+
+    @Mock
+    private UsuarioMapper mockUsuarioMapper;
+
+    @Autowired
     private UsuarioForm usuarioForm;
 
-    @Mock
+    @Autowired
     private UsuarioFormPerfil usuarioFormPerfil;
 
-    @Mock
+    @Autowired
+    private Usuario usuario;
+
+    @Autowired
     private UsuarioMapper usuarioMapper;
+
+    private List listUsuarios;
+
+    @BeforeEach
+    private void inicializar(){
+        usuarioForm.setEmailUsuario(EMAIL_CADASTRO_UPPER);
+        usuarioFormPerfil.setEmailUsuario(EMAIL_CADASTRO_UPPER);
+        listUsuarios = new ArrayList();
+    }
 
     @Test
     @DisplayName("deveEfetuarConversaoParaDTO: Deve efetuar a conversao de Usuario para UsuaioDTO")
     public void deveEfetuarConversaoDeUsuarioParaUsuarioDTO(){
-        when(usuarioMapper.converteParaDTO(any(Usuario.class))).thenReturn(new UsuarioDTO());
-        assertEquals(UsuarioDTO.class, usuarioMapper.converteParaDTO(usuario).getClass());
+        when(mockUsuarioMapper.converteParaDTO(any(Usuario.class))).thenReturn(new UsuarioDTO());
+        assertEquals(UsuarioDTO.class, mockUsuarioMapper.converteParaDTO(mockUsuario).getClass());
     }
 
     @Test
-    @DisplayName("naoDeveEfetuarConversaoDeUsuarioParaUsuarioDTO: Não deve efetuar a conversao de Usuario para UsuaioDTO")
-    public void naoDeveEfetuarConversaoDeUsuarioParaUsuarioDTO(){
-        when(usuarioMapper.converteParaDTO(any(Usuario.class))).thenReturn(null);
-        assertNull(usuarioMapper.converteParaDTO(usuario));
+    @DisplayName("deveEfetuarConversaoDeUsuarioParaUsuarioDTONulo: Deve efetuar a conversao de Usuario para UsuaioDTO nulo")
+    public void deveEfetuarConversaoDeUsuarioParaUsuarioDTONulo(){
+        when(mockUsuarioMapper.converteParaDTO(any(Usuario.class))).thenReturn(null);
+        assertNull(mockUsuarioMapper.converteParaDTO(mockUsuario));
     }
 
     @Test
     @DisplayName("deveEfetuarConversaoDeUsuarioFormParaEntity: Deve efetuar a conversao de UsuarioForm para Usuario")
     public void deveEfetuarConversaoDeUsuarioFormParaEntity(){
-        UsuarioForm uFm = new UsuarioForm();
-        uFm.setEmailUsuario(EMAIL_CADASTRO_UPPER);
-
-        Usuario usuario = new UsuarioMapper().converteParaEntity(uFm, "teste");
+        usuario = usuarioMapper.converteParaEntity(usuarioForm, "teste");
 
         assertTrue(usuario instanceof Usuario);
         assertEquals("teste", usuario.getSenhaAcessoUsuario());
@@ -74,19 +94,16 @@ public class UsuarioMapperTest {
     }
 
     @Test
-    @DisplayName("naoDeveEfetuarConversaoDeUsuarioFormParaEntity: Não deve efetuar a conversao de UsuarioForm para Usuario")
-    public void naoDeveEfetuarConversaoDeUsuarioFormParaEntity(){
-        when(usuarioMapper.converteParaEntity(any(UsuarioForm.class), any(String.class))).thenReturn(null);
-        assertNull(usuarioMapper.converteParaEntity(usuarioForm, "teste"));
+    @DisplayName("deveEfetuarConversaoDeUsuarioFormParaEntityNulo: Deve efetuar a conversao de UsuarioForm para Usuario nulo")
+    public void deveEfetuarConversaoDeUsuarioFormParaEntityNulo(){
+        when(mockUsuarioMapper.converteParaEntity(any(UsuarioForm.class), any(String.class))).thenReturn(null);
+        assertNull(mockUsuarioMapper.converteParaEntity(usuarioForm, "teste"));
     }
 
     @Test
     @DisplayName("deveEfetuarConversaoDeUsuarioperfilParaEntity: Deve efetuar a conversao de UsuarioFormPerfil para Usuario")
     public void deveEfetuarConversaoDeUsuarioFormPerfilParaEntity(){
-        UsuarioFormPerfil uFP = new UsuarioFormPerfil();
-        uFP.setEmailUsuario(EMAIL_CADASTRO_UPPER);
-
-        Usuario usuario = new UsuarioMapper().converteParaEntity(uFP, "teste");
+        usuario = usuarioMapper.converteParaEntity(usuarioFormPerfil, "teste");
 
         assertTrue(usuario instanceof Usuario);
         assertEquals("teste", usuario.getSenhaAcessoUsuario());
@@ -95,26 +112,25 @@ public class UsuarioMapperTest {
     }
 
     @Test
-    @DisplayName("naoDeveEfetuarConversaoDeUsuarioFormPerfilParaEntity: Não deve efetuar a conversao de UsuarioFormPerfil para Usuario")
-    public void naoDeveEfetuarConversaoDeUsuarioFormPerfilParaEntity(){
-        when(usuarioMapper.converteParaEntity(any(UsuarioFormPerfil.class), any(String.class))).thenReturn(null);
-        assertNull(usuarioMapper.converteParaEntity(new UsuarioFormPerfil(), "teste"));
+    @DisplayName("deveEfetuarConversaoDeUsuarioFormPerfilParaEntityNulo: Deve efetuar a conversao de UsuarioFormPerfil para Usuario nulo")
+    public void deveEfetuarConversaoDeUsuarioFormPerfilParaEntityNulo(){
+        when(mockUsuarioMapper.converteParaEntity(any(UsuarioFormPerfil.class), any(String.class))).thenReturn(null);
+        assertNull(mockUsuarioMapper.converteParaEntity(new UsuarioFormPerfil(), "teste"));
     }
 
     @Test
     @DisplayName("deveEfetuarConversaoDePageUsuarioParaUsuarioDTO: Deve efetuar a conversao de Page<Usuario> para UsuaioDTO")
     public void deveEfetuarConversaoDePageUsuarioParaUsuarioDTO(){
-        List listUsuarios = new ArrayList();
         Page<UsuarioDTO> usuariosPage = new PageImpl<UsuarioDTO>(listUsuarios);
-        when(usuarioMapper.converteParaDTO(any(Page.class))).thenReturn(usuariosPage);
-        assertTrue(usuarioMapper.converteParaDTO(usuarios) instanceof Page);
+        when(mockUsuarioMapper.converteParaDTO(any(Page.class))).thenReturn(usuariosPage);
+        assertTrue(mockUsuarioMapper.converteParaDTO(mockUsuarios) instanceof Page);
     }
 
     @Test
-    @DisplayName("naoDeveEfetuarConversaoDePageUsuarioParaUsuarioDTO: Não deve efetuar a conversao de Page<Usuario> para UsuaioDTO")
-    public void naoDeveEfetuarConversaoDePageUsuarioParaUsuarioDTO(){
-        when(usuarioMapper.converteParaDTO(any(Page.class))).thenReturn(null);
-        assertNull(usuarioMapper.converteParaDTO(usuarios));
+    @DisplayName("deveEfetuarConversaoDePageUsuarioParaUsuarioDTONulo: Deve efetuar a conversao de Page<Usuario> para UsuaioDTO nulo")
+    public void deveEfetuarConversaoDePageUsuarioParaUsuarioDTONulo(){
+        when(mockUsuarioMapper.converteParaDTO(any(Page.class))).thenReturn(null);
+        assertNull(mockUsuarioMapper.converteParaDTO(mockUsuarios));
     }
 
 
